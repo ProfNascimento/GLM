c3951b87e76f79631cf743ee0a818cbe3273fd35
Diego Nascimento <91238394+ProfNascimento@users.noreply.github.com>
2021-11-24 23:26:14 -0300

    Class 29/11

--- Poisson_REG.R
+++ Poisson_REG.R
@@ -3,6 +3,7 @@
 ####################
 set.seed(9)
 sampDB=rpois(100,lambda = 4)
+hist(sampB)
 
 # MLE of the POISSON PARAMETER
 mean(sampDB)
@@ -31,7 +32,7 @@ shipsf$type = factor(shipsf$type)
 shipsf$year = factor(shipsf$year)
 shipsf$period = factor(shipsf$period)
 
-hist(shipsf$incidents/shipsf$service)
+#hist(shipsf$incidents/shipsf$service)
 
 library(GGally)
 ggpairs(shipsf, columns = c("incidents","type","year",
@@ -44,20 +45,24 @@ mod1 = glm(incidents ~ type + year + period
            data=shipsf)
 
 # Checking the assumption [VAR(Y) = E(Y)]
-pchisq(mod1$deviance,mod1$df.residual,lower.tail = FALSE)
+pchisq(mod1$deviance, mod1$df.residual, lower.tail = FALSE)
 
 summary(mod1)
 
 require(jtools)
 # plot regression coefficients for mod1
-plot_summs(mod1, scale = TRUE, exp = TRUE)
+plot_summs(mod1, scale = TRUE, exp = TRUE,ci_level = 0.95)
 cat_plot(mod1, pred = period, modx = type)
 
 ## WITHOUT OFFSET
-poisson.model2=glm(incidents ~ type + year + period, family=poisson,
+poisson.model2=glm(incidents ~ type + year + period, 
+                   family=poisson,
                 control=glm.control(epsilon=0.0001,maxit=100),
                 data=shipsf)
+
 summary(poisson.model2) #Violation VAR(Y) not equal E(Y)
+pchisq(poisson.model2$deviance, poisson.model2$df.residual, lower.tail = FALSE)
+
 plot_summs(mod1, poisson.model2, scale = TRUE, exp = TRUE)
 
 
@@ -65,7 +70,7 @@ plot_summs(mod1, poisson.model2, scale = TRUE, exp = TRUE)
 # Suppose your fitted model is modresiduals(mod,type = "response") ## response residuals
 residuals(mod1,type = "pearson") ## pearson residuals
 residuals(mod1,type = "deviance") ## deviance residuals
-residuals(mod1,type = "deviance") ## pearson residuals
+residuals(mod1,type = "response") ## response residuals
 
 # residuals against estimates (mu_i) first
 plot(residuals(mod1) ~ predict(mod1,type="response"),xlab=expression(hat(mu)),ylab="Deviance residuals",pch=20,col="red")
@@ -97,16 +102,18 @@ faraway::halfnorm((c_d)) #Cook's distance
 plot(mod1)
 
 # make a dataframe with new data
-newdata = data.frame(type="B", year="65", period="60",service=1000)
+newdata222 = data.frame(type="B", year="65", 
+                     period="60",service=1000)
 # use 'predict()' to run model on new data
-predict(mod1, newdata = newdata, type = "response")
+predict(mod1, newdata = newdata222, type = "response")
 
 
 ######################
 ## REAL APPLICATION ##
 ######################
+require(ggplot2)
 require(datasets)
-data < - warpbreaks
+data <- warpbreaks
 #https://stat.ethz.ch/R-manual/R-devel/library/datasets/html/warpbreaks.html
 
 names(data) # Extract column names from dataframe
@@ -114,18 +121,41 @@ str(data)
 
 hist(data$breaks)
 
-#library(GGally)
-#ggpairs(data, columns = c("breaks","wool","tension"))
+ggplot(data, aes(breaks, fill = wool)) + 
+  geom_histogram(binwidth = 1) + facet_grid(tension ~ ., 
+  margins = TRUE, scales = "free")
+
+with(data, tapply(breaks, tension, function(x) {
+  sprintf("M (SD) = %1.2f (%1.2f)", mean(x), sd(x))
+}))
 
 mean(data$breaks) # calculate mean
 var(data$breaks) # calculate variance
 
 # model poisson regression using glm()
-poisson.model <- glm(breaks ~ wool + tension, data, family = poisson(link = "log"))
+poisson.model <- glm(breaks ~ wool*tension, data, family = poisson(link = "log"))
 summary(poisson.model)
 
 pchisq(poisson.model$deviance,
        poisson.model$df.residual,lower.tail = FALSE)
 
-poisson.model2 <- glm(breaks ~ wool + tension, data = data, family = quasipoisson(link = "log"))
-summary(poisson.model2)
+BN.model <- MASS::glm.nb(breaks ~ wool*tension, data = data)
+summary(BN.model)
+
+pchisq(BN.model$deviance,
+       BN.model$df.residual,lower.tail = FALSE)
+
+require(jtools)
+cat_plot(BN.model, pred = tension, modx = wool)
+plot_summs(BN.model, scale = TRUE, exp = TRUE,ci_level = 0.95)
+
+plot(BN.model)
+
+AIC(poisson.model);AIC(BN.model)
+BIC(poisson.model);BIC(BN.model)
+
+#Likelihood ratio test to compare these two and test model assumption
+pchisq(2 * (logLik(BN.model) - logLik(poisson.model)), df = 1, lower.tail = FALSE)
+# This strongly suggests the negative binomial model, 
+# estimating the dispersion parameter, 
+# is more appropriate than the Poisson model.
